b08056634854ae0004b5dc34d8944c6122590ca9
Ramon Olea <50149082+Ramon-Olea@users.noreply.github.com>
2022-12-09 11:39:11 -0600

    primera carga

---
+++ app/Controllers/Home.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index()
+    {
+        return view('login');
+    }
+
+    public function inicio()
+    {
+        return view('inicio');
+    }
+}
